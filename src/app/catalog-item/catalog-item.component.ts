8f3fddadedf9433ab96b72885e7f5df955fb8f72
junhuabrave <junhuabrave@gmail.com>
2025-08-16 12:39:53 +0100

    add fastapi and components commit

---
+++ src/app/catalog-item/catalog-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog-item',
+  imports: [],
+  templateUrl: './catalog-item.component.html',
+  styleUrl: './catalog-item.component.scss'
+})
+export class CatalogItemComponent {
+
+}
---
+++ src/app/version-info/version-info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-version-info',
+  imports: [],
+  templateUrl: './version-info.component.html',
+  styleUrl: './version-info.component.scss'
+})
+export class VersionInfoComponent {
+
+}
